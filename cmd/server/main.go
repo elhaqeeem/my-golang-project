0ba94f2ebc4de3deecaa5ecb0de1753d3f5fc735
Lukmanul Hakim <manglayangselalu@gmail.com>
2024-12-17 20:30:57 +0700

    update func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,8 +27,8 @@ func findAvailablePort() (string, error) {
 
 func main() {
 	// Inisialisasi Gin
-	r := gin.Default()
 	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 	// Route HTTP/3 utama
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello HTTP/3!")
